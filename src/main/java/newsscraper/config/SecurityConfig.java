32fc5a38f74325b4da9b292d68d4e5f38f83536a
hodokim <hodo323@naver.com>
2025-08-05 22:09:16 +0900

    포트 수정

--- src/main/java/newsscraper/config/SecurityConfig.java
+++ src/main/java/newsscraper/config/SecurityConfig.java
@@ -5,13 +5,13 @@ import lombok.RequiredArgsConstructor;
 import newsscraper.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod; // HttpMethod 임포트 추가
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy; // SessionCreationPolicy 임포트 추가
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
